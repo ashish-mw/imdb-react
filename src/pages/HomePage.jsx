93a0eb25a805c9d9ba6c96ce17e9dac222632688
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-03-09 13:20:18 +0530

    adding axios calls

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Page from "../components/Page";
 
+import { apiGetMovies } from "../services/api";
+
 const HomePage = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const request = axios.CancelToken.source();
+    async function getMovies() {
+      try {
+        const response = await apiGetMovies({ cancelToken: request.token });
+        setMovies(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    getMovies();
+    return () => {
+      request.cancel();
+    };
+  }, []);
+
   return (
     <Page title="Home">
-      <h1>Home page</h1>
+      <h1>All movies</h1>
+      {movies.map((m) => (
+        <div key={m.id}>
+          <p>
+            {m.name} - {m.year}
+          </p>
+          <p>{m.genre}</p>
+          <p>{m.rating} stars</p>
+        </div>
+      ))}
     </Page>
   );
 };
